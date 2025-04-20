5f4501ee919f390750fa84d155b8c7019683fc4d
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-04-20 21:41:31 +0500

    Student Progress Tracker
    Completed

--- mern-student-tracker/frontend/src/App.jsx
+++ mern-student-tracker/frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Student from "./components/Student";
+import Studentform from "./components/Studentform";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/students")
+      .then((res) => setStudents(res.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const addStudent = (student) => {
+    axios
+      .post("http://localhost:5000/students", student)
+      .then(() => {
+        setStudents([...students, student]);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const updateProgress = (name) => {
+    const student = students.find((s) => s.name === name);
+    const newProgress = student.progress + 5;
+    axios
+      .put(`http://localhost:5000/students/${name}`, { progress: newProgress })
+      .then(() => {
+        setStudents(
+          students.map((s) =>
+            s.name === name ? { ...s, progress: newProgress } : s
+          )
+        );
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Student Progress Tracker</h1>
+      <Studentform addStudent={addStudent} />
+      {students.map((student) => (
+        <Student
+          key={student.name}
+          name={student.name}
+          age={student.age}
+          progress={student.progress}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
