897930a071222a6c6ddceb51a3bba746583739b1
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-04-28 22:40:29 +0500

    Student Progress Tracker

--- mern-student-tracker/frontend/src/components/Student.jsx
+++ mern-student-tracker/frontend/src/components/Student.jsx
@@ -1,6 +1,7 @@
 function Student({ name, progress, updateProgress }) {
   return (
     <div>
+      <h1>Student Detail</h1>
       <h3>{name}</h3>
       <p>Progress: {progress}</p>
       <button onClick={() => updateProgress(name)}>Increase Progress</button>
